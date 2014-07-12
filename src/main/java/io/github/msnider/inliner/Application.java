b4134f2cbc2e0fc3140f235c73cc8d3751f33e57
Matt Snider <msnider@ucla.edu>
2014-07-12 11:38:50 -0700

    Prepared for deployment on Heroku

--- src/main/java/io/github/msnider/inliner/Application.java
+++ src/main/java/io/github/msnider/inliner/Application.java
@@ -13,6 +13,6 @@ import org.springframework.context.annotation.Configuration;
 public class Application {
 	public static void main(String[] args) throws Exception {
 		SSLUtils.setTrustAllCerts();
-        SpringApplication.run(Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
